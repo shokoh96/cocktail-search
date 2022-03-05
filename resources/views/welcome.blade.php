b2dc75746b1eca105ea914f51b75f0a447935e02
Mami <amistade.s-r-k.000-0039@hotmail.co.jp>
2022-03-05 14:59:14 +0900

    タイトル変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    カクテル③
                 </div>
 
                 <div class="links">
