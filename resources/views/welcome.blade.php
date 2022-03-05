f197e0209b9de10459df7a15efe0ce0c4d372db8
遠藤大樹 <dai.laugh.end.0925@gmail.com>
2022-03-05 14:59:09 +0900

    タイトル変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    カクテル1
                 </div>
 
                 <div class="links">
