72f9c817f8686be060941b70b9d3fc18807d13f1
KOH <gaosheng087@gmail.com>
2022-03-22 11:39:40 +0900

    モデル・マイグレーション作成

---
+++ database/migrations/2022_03_22_013551_create_memos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('base')->nullable();
+            $table->string('taste')->nullable();
+            $table->string('feature')->nullable();
+            $table->text('comment')->nullable();
+            $table->string('image')->default('no-image');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memos');
+    }
+}
