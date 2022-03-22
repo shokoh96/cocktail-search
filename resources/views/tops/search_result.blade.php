3d8ae4627cacd120d82cad8e278ff535bf641311
genki <99254762+genki409@users.noreply.github.com>
2022-03-22 11:39:46 +0900

    検索結果追加

---
+++ resources/views/tops/search_result.blade.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>検索結果</title>
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="search_result.css">
+    <link rel="stylesheet" href="Senobi-Gothic-Medium.ttf">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/7d803bff65.js" crossorigin="anonymous"></script>
+    
+
+
+</head>
+<body>
+    <header class="header"  id="page-top">
+        <ul class="item">
+            <li><a class="menu" href="">新規登録</a></li>
+            <li><a class="menu2" href="">ログイン</a></li>
+            <li><a class="menu3" href="">お酒検索</a></li>
+            <li><a class="menu4" href="">記録一覧</a></li>
+        </ul>
+    </header>
+    <main class="main">
+        <div class="main-top">
+
+        </div>
+        <div class="pagination pagination-top">
+            <div class="pagination-inner">
+              <div class="pagination-page">
+                <strong>
+                  <span>1</span>
+                </strong>
+                <a href="#">
+                  <span>2</span>
+                </a>
+                <a href="#">
+                  <span>3</span>
+                </a>
+                <a href="#">
+                  <span>4</span>
+                </a>
+                <a href="#">
+                  <span>5</span>
+                </a>
+                <span class="pagination-omission">…</span>
+              </div><!-- /.pagination_page -->
+              <div class="pagination-next">
+                <a href="#">
+                  <span>次</span>
+                  <i class="fas fa-angle-right"></i>
+                </a>
+              </div><!-- /.pagination_next-->
+              <div class="pagination-last">
+                <a href="#">
+                  <span>最後</span>
+                  <i class="fas fa-angle-double-right"></i>
+                </a>
+              </div><!-- /.pagination_last -->
+            </div><!-- /.pagination-inner -->
+          </div><!-- /.pagination-top -->
+        <div class="main-middle">
+          <a href="#">
+            <div class="card">
+                <img src="ジントニック.jpeg" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h2 class="japanese">ジントニック</h2>
+                    <h4 class="english">Gin Tonik</h4>
+                </div>
+            </div>
+          </a>
+          <a href="#">
+            <div class="card">
+                  <img src="ジントニック.jpeg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                      <h2 class="japanese">ジントニック</h2>
+                      <h4 class="english">Gin Tonik</h4>
+                  </div>
+            </div>
+          </a>
+          <a href="#">
+            <div class="card">
+                <img src="ジントニック.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h2 class="japanese">ジントニック</h2>
+                    <h4 class="english">Gin Tonik</h4>
+                </div>
+            </div>
+          </a>
+          <a href="#">
+            <div class="card">
+                <img src="ジントニック.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h2 class="japanese">ジントニック</h2>
+                  <h4 class="english">Gin Tonik</h4>
+                </div>
+            </div>
+          </a>
+          <a href="#">
+            <div class="card">
+                <img src="ジントニック.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h2 class="japanese">ジントニック</h2>
+                    <h4 class="english">Gin Tonik</h4>
+                </div>
+            </div>
+          </a>
+          <a href="#">
+            <div class="card">
+                <img src="ジントニック.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h2 class="japanese">ジントニック</h2>
+                    <h4 class="english">Gin Tonik</h4>
+                </div>
+            </div>
+          </a>
+        </div>
+        <div class="pagination pagination-bottom">
+            <div class="pagination-inner">
+              <div class="pagination-page">
+                <strong>
+                  <span>1</span>
+                </strong>
+                <a href="#">
+                  <span>2</span>
+                </a>
+                <a href="#">
+                  <span>3</span>
+                </a>
+                <a href="#">
+                  <span>4</span>
+                </a>
+                <a href="#">
+                  <span>5</span>
+                </a>
+                <span class="pagination-omission">…</span>
+              </div><!-- /.pagination_page -->
+              <div class="pagination-next">
+                <a href="#">
+                  <span>次</span>
+                  <i class="fas fa-angle-right"></i>
+                </a>
+              </div><!-- /.pagination_next-->
+              <div class="pagination-last">
+                <a href="#">
+                  <span>最後</span>
+                  <i class="fas fa-angle-double-right"></i>
+                </a>
+              </div><!-- /.pagination_last -->
+            </div><!-- /.pagination-inner -->
+          </div><!-- /.pagination-bottom -->
+        <!-- ページトップボタン -->
+        <div class="main-bottom">
+            <a href="#page-top"><img class="top-btn-img" src="top.png" alt=""></a>
+        </div>
+    </main>
+    <footer class="footer">
+        <h3>ここ酒</h3>
+        <p>copyright &copy; 2022 the 5th class All Rights Reserved</p>
+    </footer>
+</body>
+</html>
