322690829cb882597695c7b6973d4243cccbb669
Kakeru0720 <fukuikakeru0720@gmail.com>
2022-03-24 20:50:51 +0900

    ルート削除

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/search_show', function () {
-    return view('tops.search_show');
-});
