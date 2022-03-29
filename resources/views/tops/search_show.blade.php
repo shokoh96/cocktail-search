abef2513549347865c2846d1110f91d110354502
KOH <gaosheng087@gmail.com>
2022-03-30 01:17:43 +0900

    API個別詳細画面実装

--- resources/views/tops/search_show.blade.php
+++ resources/views/tops/search_show.blade.php
@@ -1,82 +1,75 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/search_show.css">
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/layouts.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/search_show.css') }}">
+
 </head>
 @extends('layouts.layouts')
 @section('content')
-    
-<body>
 
-     <div class="guid">
-         <div class="front"><< 前へ</div>
-         <div class="back">戻る</div>
-         <div class="next">次へ >></div>
-     </div>
+    <body>
+        <div class="guid">
+            {{-- <a class="front" href="#">
+            << 前へ</a> --}}
+            <a class="back" href="javascript:history.back()">戻る</a>
+            {{-- <a class="next" href="#">次へ >></a> --}}
+        </div><!-- /.guid -->
 
-    <div class="box">
-        <div class="box2">
-            <div class="container">
-                <p class="ex">ジンの良さをを気軽に楽しめる王道のカクテル</p> 
-                <h1 class="zin">ジン・トニック</h1>
-                <div class="container2">
+        <div class="box">
+            <div class="box2">
+                <div class="container">
+                    <p class="ex">{{ $posts['cocktail_digest'] }}</p>
+                    <h1 class="zin">{{ $posts['cocktail_name'] }}</h1>
+                    {{-- <div class="container2">
                     <div class="box3">
                         スタンダード
                     </div>
                     <div class="box4">
                         キレがある
-                    </div>  
+                    </div>
+                </div> --}}
                 </div>
+
+                <img class="picture" src="{{ asset('img/sample_zin_KH.jpeg') }}" alt="">
+
+                <div class="container3">
+                    <div class="box5">Base</div>
+                    <div class="box6">{{ $posts['base_name'] }}</div>
+                    <div class="box7">taste</div>
+                    <div class="box8">{{ $posts['taste_name'] }}</div>
+                    <div class="box9">Alc.</div>
+                    <div class="box10">{{ $posts['alcohol'] }}%</div>
+                </div>
+                @foreach ($posts['recipes'] as $recipe)
+                    <div class="container4">
+                        <div class="comp1">{{ $recipe['ingredient_name'] }}</div>
+                        <div class="comp2">{{ $recipe['amount'] }}{{ $recipe['unit'] }}</div>
+                    </div>
+                    <hr class="hr1">
+                @endforeach
+
+                <p class="ex2">{{ $posts['recipe_desc'] }}</p>
+
+                <hr class="hr3">
+
+                <p class="ex3">{{ $posts['cocktail_desc'] }}</p>
+
             </div>
-
-             <img class="picture" src="img/sample_zin_KH.jpeg" alt=""> 
-
-             <div class="container3">
-                 <div class="box5">Base</div>
-                 <div class="box6">ジン</div>
-                 <div class="box7">taste</div>
-                 <div class="box8">中辛口</div>
-                 <div class="box9">Alc.</div>
-                 <div class="box10">16%</div>
-             </div>
-
-             <div class="container4">
-                 <div class="comp1">ジン</div>
-                 <div class="comp2">45ml</div>
-             </div> 
-             <hr class="hr1"> 
-             <div class="container5">
-                <div class="comp3">トニックウォーター</div>  
-                <div class="comp4">FullUP</div>
-             </div>
-             <hr class="hr2">
-             <div class="container6">
-                <div class="comp5">ライム</div>
-                <div class="comp6">6/1個</div> 
-             </div>
-
-             <p class="ex2">グラスに氷を入れてジン注ぎ、その後冷えたトニック・ウォーターでグラスを満たし、炭酸が抜けないよう軽くステアする。最後にカットしたライムを飾り付ける。</p>
-            
-             <hr class="hr3">
-
-            <p class="ex3">ジンとトニックウォーターをビルドするシンプルなレシピ。家でも簡単に作れる上、さっぱりして飲みやすい。シンプルかつスタンダードなカクテルが故にバーテンダーの腕が出やすいとされる。なお、トニックウォーターだけでは甘すぎる場合は炭酸水を半分入れたジン・ソニックというカクテルもある。
-            </p>
-            
+            <div class="guid2">
+                {{-- <div class="front">
+                    << 前へ</div> --}}
+                <div class="back">戻る</div>
+                {{-- <div class="next">次へ >></div> --}}
+            </div>
         </div>
-
-        <div class="guid2">
-            <div class="front"><< 前へ</div>
-            <div class="back">戻る</div>
-            <div class="next">次へ >></div>
-        </div>
-    
-    
-    </div>
-</body>
+    </body>
 @endsection
+
 </html>
