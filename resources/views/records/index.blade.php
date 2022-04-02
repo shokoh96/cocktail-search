b549c1644f2c7b0caad347fbf44dc492ff3d32dc
KOH <gaosheng087@gmail.com>
2022-04-02 12:30:17 +0900

    リンク修正

--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -96,7 +96,7 @@
                     <div class="cards">
                         <div class="row">
                             @foreach ($records as $record)
-                                <a href="#" class="col-lg-3 col-md-6">
+                                <a href="{{ route('records.show', $record->id) }}" class="col-lg-3 col-md-6">
                                     <div class="card">
                                         <img src="{{ $record->image }}" alt="" class="card-img">
                                         {{-- <img src="{{ asset('img/Sample-img.jpg') }}" alt="" class="card-img"> --}}
